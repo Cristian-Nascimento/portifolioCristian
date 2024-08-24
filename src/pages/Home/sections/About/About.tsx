25ef2cb6047fea6261e8ac7b3afb8ec6dec91b64
cristian Nascimento <cristian.nascimento@enacom.com.br>
2024-08-24 18:12:01 -0300

    feature: add new version

--- src/pages/Home/sections/About/About.tsx
+++ src/pages/Home/sections/About/About.tsx
@@ -4,17 +4,23 @@ import './About.css'
 const About = () => {
     return (
         <section className="about" id="about">
-        <div className="about-content">
-            <h2 className="headingAbout">Sobre Mim</h2>
-            <p>
-                Olá me chamo Cristian, sou um gafanhato apaixonado por programação.
-                começei nesse mundo enquanto cursava Engenharia de produção, hoje quase formando em Sistemas de Informação com um pouco mais de 2 anos de experiência, continuo a buscar e aprender novas tecnologias e ferramentas para me tornar um Dev Full Stack. 
-            </p>
-        </div>
-        <div className="about-img">
-            <img src={fotoPerfil} alt="Imagem de perfil"/>
-        </div>
-    </section>
+            <div className="about-content">
+                <h2 className="headingAbout">Conheça um pouco sobre mim</h2>
+                <p>
+                    Oi, sou o Cristian, um entusiasta de programação!
+                    Comecei nessa jornada enquanto cursava Engenharia de Produção e,
+                    hoje, quase formando em Sistemas de Informação, que coisa neh! :)
+                </p>
+                <p>
+                    Além disso, sou apaixonado por aprender coisas novas,
+                    sempre buscando me aprimorar e me manter atualizado com as novas tecnologias.
+                    Sempre disposto a ajudar e a aprender com os outros.
+                </p>
+            </div>
+            <div className="about-img">
+                <img src={fotoPerfil} alt="Imagem de perfil" />
+            </div>
+        </section>
     )
 }
 
--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,18 +1,45 @@
-import fundo from '../../../../assets/fundo branco perfil.png'
+import { useLayoutEffect, useRef } from 'react'
+import fundo from '../../../../assets/fundo preto perfil.png'
 import './Hero.css'
+import { gsap } from 'gsap';
 
 const Hero = () => {
-    return (
-        <section className="home_home" id="home">
-            <div className="home-img">
-                <img src={fundo} alt="Imagem de perfil" className='img'/>
-            </div>
+    const el = useRef<HTMLDivElement>(null);
+    const tl = useRef<gsap.core.Timeline>();
 
+    useLayoutEffect(() => {
+        gsap.context(() => {
+            tl.current = gsap.timeline({
+                scrollTrigger: {
+                    trigger: '.home_home',
+                    start: 'top center',
+                }
+            })
+            .fromTo('.home-content, .home-img', {
+                opacity: 0,
+                x: 160,
+            }, {
+                opacity: 1,
+                x: 0,
+                duration: 2
+            })
+        }, el);
+    
+        return () => {
+            gsap.killTweensOf('.home_home');
+        }
+    }, []);
+    return (
+        <section className="home_home" id="home" ref={el}>
             <div className="home-content">
-                <h3>Olá seja bem vindo</h3>
+                <h3>DESENVOLVEDOR BACK-END</h3>
+                <br></br>
                 <p>
-                    Sou desenvolvedor Backend utilizando as tecnologias Node.js, Express.js e MongoDB.
+                    Com 2 anos de experiência em Node.js, MongoDB e Express, tenho capacidade de construir aplicações
+                    escaláveis e de alta performance, aplicando metodologias ágeis,
+                    clean code e TDD. Expertise em Docker e AWS, e versionamento de código Git.
                 </p>
+
 
                 <div className="social-icons">
                     <a href="https://www.linkedin.com/in/cristian-rosa-nascimento/" target="_blank"><i
@@ -21,6 +48,10 @@ const Hero = () => {
                     <a href="https://www.instagram.com/cristian._nascimento/" target="_blank"><i
                         className='bx bxl-instagram'></i></a>
                 </div>
+            </div>
+
+            <div className="home-img">
+                <img src={fundo} alt="Imagem de perfil" className='img' />
             </div>
         </section>
     )
